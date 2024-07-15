e9c730093779b9f2514076d58f351fdd0773b112
Amit Sharma <amitsharma@MacBook-Pro.local>
2024-07-15 14:46:56 -0700

    React Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>This space is for Frontend Excercises :)</p>
+    </div>
+  );
+}
